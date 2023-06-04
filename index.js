832fc95af72597d35705293746ff7c233e31480d
Rick Vermeil <rickv85@gmail.com>
2023-06-04 09:52:28 -0400

    Change back to mysql

--- index.js
+++ index.js
@@ -1,61 +1,65 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 5001;
-
-const { Pool } = require("pg");
 
 const cors = require("cors");
 
 
-let sslOptions = null;
+const url = require('url');
+const dbUrl = new URL(process.env.DATABASE_URL);
 
-if (process.env.NODE_ENV === "production") {
-  sslOptions = {
-    rejectUnauthorized: false,
-  };
-}
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslOptions,
+const pool = mysql.createPool({
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.substr(1),
+  connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-express()
-  .use(express.json())
-  .use(
-    express.urlencoded({
-      extended: true,
-    })
-  )
-  .use(
-    cors({
-      origin: ["http://localhost:3000"],
-    })
-  )
-  .use(express.static(path.join(__dirname, "public")))
-  .get("/", async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const tableNamesResult = await client.query(
-        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-      );
-      const tableNames = tableNamesResult.rows.map((row) => row.table_name);
 
-      const results = {};
 
-      for (const tableName of tableNames) {
-        const query = `SELECT * FROM ${tableName}`;
-        const result = await client.query(query);
-        results[tableName] = result.rows;
-      }
+const app = express();
 
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+);
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+  try {
+    const connection = await pool.promise().getConnection();
+    const [rows] = await connection.query("SHOW TABLES");
+
+    const tableNames = rows.map((row) => row[`Tables_in_${process.env.MYSQL_DATABASE}`]);
+
+    const results = {};
+
+    for (const tableName of tableNames) {
+      const [rows] = await connection.query(`SELECT * FROM ${tableName}`);
+      results[tableName] = rows;
+    }
+
+    res.json(results);
+
+    connection.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
