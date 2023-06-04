16fec95fecc6fee6a0ec3e5204607e534b718b01
Rick Vermeil <rickv85@gmail.com>
2023-06-04 08:45:58 -0400

    Change to Heroku/Postgres

--- index.js
+++ index.js
@@ -1,106 +1,60 @@
 const express = require("express");
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config();
 
+const PORT = process.env.PORT || 5001;
 
-const mysql = require('mysql2');
-const app = express();
+const { Pool } = require("pg");
+
 const cors = require("cors");
 
-const password = process.env.DB_PASSWORD;
+// app.use(express.json());
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+// app.use(cors({
+//   origin: ['http://localhost:3000']
+// }));
 
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-app.use(cors({
-  origin: ['http://localhost:3000']
-}));
-
-
-const pool = mysql.createPool({
-  host: '34.70.242.8',
-  user: 'root',
-  password: `${password}`,
-  database: 'data',
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
 });
 
-app.get('/data', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error connecting to database:', err);
-      return res.status(500).json({ error: 'Error connecting to database' });
-    }
+express()
+  .use(express.json())
+  .use(
+    express.urlencoded({
+      extended: true,
+    })
+  )
+  .use(
+    cors({
+      origin: ["http://localhost:3000"],
+    })
+  )
+  .use(express.static(path.join(__dirname, "public")))
+  .get("/", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const tableNamesResult = await client.query(
+        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+      );
+      const tableNames = tableNamesResult.rows.map((row) => row.table_name);
 
-    console.log('Connected to database.');
+      const results = {};
 
-    // Fetch table names
-    const showTablesQuery = 'SHOW TABLES';
-    connection.query(showTablesQuery, (error, results) => {
-      if (error) {
-        console.error('Error fetching table names:', error);
-        connection.release();
-        return res.status(500).json({ error: 'Error fetching table names' });
+      for (const tableName of tableNames) {
+        const query = `SELECT * FROM ${tableName}`;
+        const result = await client.query(query);
+        results[tableName] = result.rows;
       }
 
-      const tableNames = results.map((row) => Object.values(row)[0]);
-
-      const dataPromises = tableNames.map((tableName) => {
-        return new Promise((resolve, reject) => {
-          const selectQuery = `SELECT * FROM ${tableName}`;
-          connection.query(selectQuery, (error, results) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve({ tableName, data: results });
-            }
-          });
-        });
-      });
-
-      Promise.all(dataPromises)
-        .then((tableData) => {
-          connection.release();
-          res.json(tableData);
-        })
-        .catch((error) => {
-          console.error('Error fetching table data:', error);
-          connection.release();
-          res.status(500).json({ error: 'Error fetching table data' });
-        });
-    });
-  });
-});
-
-app.post('/users', (req, res) => {
-  const user = req.body;
-  console.log(user)
-
-  // Test for slower connections and set timeout here?
-  // Slow connection was causing this post to fail - 500
-
-  // Check if the user exists
-  pool.query('SELECT * FROM users WHERE userId = ?', [user.userId], (err, results) => {
-    if (err) {
-      console.error("Error:", err);
-      return res.status(500).json({ error: 'Error connecting to database' });
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
     }
-    
-    // If the user doesn't exist, insert the new user
-    if (results.length === 0) {
-      pool.query('INSERT INTO users SET ?', user, (err, result, fields) => {
-        if(err) {
-          console.error("Error:", err);
-          return res.status(500).json({ error: 'Error inserting user into database' });
-        }
-        res.status(201).send('User added successfully');
-      });
-    } else {
-      res.status(409).send('Database connection successful. User already exists.');
-    }
-  });
-});
-
-
-app.listen(process.env.PORT || 3001, () =>
-  console.log('App running on http://localhost:3001')
-);
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
