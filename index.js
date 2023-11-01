248bb6b723d46838946d3777ff4e346818b5b792
Rick Vermeil <rickv85@gmail.com>
2023-11-01 17:24:18 -0600

    Secure true

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   // Cookie needs to be secure false when running on local env
-  cookie: { secure: false, sameSite: "none" }
+  cookie: { secure: true, sameSite: "none" }
 }));
 
 // May want to move CSP to Lusca below and delete Helmet
