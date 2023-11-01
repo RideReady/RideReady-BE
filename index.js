0786ae0f9f2d7b6ebcd83aaa9be39fb268dd871e
Rick Vermeil <rickv85@gmail.com>
2023-11-01 16:45:29 -0600

    Change samesite to none

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   // Changed cookie to secure true, local is http so will need to be set to false
-  cookie: { secure: true, sameSite: "strict" }
+  cookie: { secure: true, sameSite: "none" }
 }));
 
 // May want to move CSP to Lusca below and delete Helmet
